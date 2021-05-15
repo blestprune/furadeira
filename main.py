781b4acf6f82a74f617942816b3f3ecebb05101a
Heitor de Carvalho <63377584+blestprune@users.noreply.github.com>
2021-05-15 12:55:49 -0300

    Troca client por bot e adiciona slash commands

--- main.py
+++ main.py
@@ -1,119 +1,159 @@
-import discord
 import random
 import os
-import motos
+import listas
 import requests
-from replit import db
-from servidor import servidor
 
-client = discord.Client()
+import sqlalchemy_declarative
+import database as db
+import discord
+from discord import Intents
+from discord.ext import commands
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 
-filetypes = [".JPG", ".jpg", ".PNG", ".png"]
+FILETYPES = [".JPG", ".jpg", ".PNG", ".png"]
 
-if "responding" not in db.keys():
-    db["responding"] = True
+intents = Intents.all()
+bot = commands.Bot(command_prefix="!",
+                   description='Furadeira',
+                   intents=intents)
 
+slash = SlashCommand(bot, sync_commands=True)
+
+
+# ----- Utils -----------------------------------------------------------------
 
 def random_bike():
-    bike = random.choice(motos.modelos)
-    embed_msg = discord.Embed(title=bike['modelo'], description=bike['info'], color=0xff6600)
-    # embed_msg.add_field(name="Descrição", value=bike['info'], inline=False)
+    bike = random.choice(listas.motos)
+    embed_msg = discord.Embed(title=bike['modelo'],
+                              description=bike['info'],
+                              color=0xff6600)
     embed_msg.set_image(url=bike['img_url'])
     return embed_msg
 
 
-def update_images(img_url):
-    if "images" in db.keys():
-        images = db["images"]
-        images.append(img_url)
-        db["images"] = images
-    else:
-        db["images"] = [img_url]
-
-
-def delete_image(index):
-    images = db["images"]
-    if len(images) > index:
-        del images[index]
-    db["images"] = images
-
-
 def list_images():
-    images = db["images"]
-    lista = ""
-    for i in range(len(images)):
-        response = requests.get(images[i])
+    frases = db.pegar_frases()
+    bugadas = ""
+    for imagem in frases:
+        response = requests.get(imagem.url)
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError:
-            lista += f"\n{i}: {images[i]}"
-    lista = "```Imagens bugadas\n" + lista + "```"
-    return lista
+            bugadas += f"\n{imagem.id}: {imagem.url}"
+    bugadas = "```Imagens bugadas\n" + bugadas + "```"
+    return bugadas
 
 
 def nota_aleatoria(aluno, cadeira):
-    nota = random.randint(0, 100) / 10
-    resultado = f"{aluno}, sua nota em {cadeira} será {round(nota, 1)}"
+    valor = random.randint(0, 100) / 10
+    resultado = f"{aluno}, sua nota em {cadeira} será {round(valor, 1)}"
     return resultado
-    
 
-@client.event
+
+# ----- Bot Events ------------------------------------------------------------
+
+@bot.event
 async def on_ready():
-    print(f'Eu sou a {client.user} e estou online.')
+    print(f'{bot.user} online.')
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
-    if db["responding"]:
-        options = []
-        if "images" in db.keys():
-            options = options + db["images"]
-        if message.content.startswith('!frase'):
-            await message.channel.send(random.choice(options))
+    if message.content.lower().startswith("kkk"):
+        await message.channel.send(random.choice(listas.risadas))
 
-    if message.content.startswith("!list"):
-        if "images" in db.keys():
-            images = list_images()
-        await message.channel.send(images)
+    if message.content.lower().startswith("bom dia"):
+        await message.channel.send("grande dia")
 
-    if message.content.startswith("!harley"):
-        msg = f'{message.author.mention}, sua Harley-Davidson é:'
-        await message.channel.send(msg, embed=random_bike())
-
-    if message.content.startswith("!fraude"):
-        msg = random.choice(motos.fraudes)
-        await message.channel.send(msg)
-
-    if message.content.startswith("!fome"):
-        msg = random.choice(motos.fome)
-        await message.channel.send(msg)
-
-    if message.content.startswith("K"):
-        msg = random.choice(motos.risadas)
-        await message.channel.send(msg)
-    
-    if message.content.startswith("!nota"):
-        aluno = message.author.mention
-        cadeira = message.content.split("!nota ")[1]
-        await message.channel.send(nota_aleatoria(aluno, cadeira))
-
-    if message.content.startswith("!add") and message.attachments:
-        for filetype in filetypes:
-            if filetype in message.attachments[0].url:
-                update_images(message.attachments[0].url)
-                await message.channel.send("Imagem adicionada.")
-
-    if message.content.startswith("!del"):
-        images = []
-        if "images" in db.keys():
-            index = int(message.content.split("!del", 1)[1])
-            delete_image(index)
-            images = db["images"]
-        await message.channel.send("Imagem removida.")
+    await bot.process_commands(message)
 
 
-servidor()
-client.run(os.getenv('TOKEN'))
+# ----- Bot Slash Commands ----------------------------------------------------
+
+@slash.slash(name="frase",
+             description="Escolho uma frase icônica aleatória bem legal")
+async def frase(ctx):
+    frases = db.pegar_frases()
+    imagem = random.choice(frases)
+    await ctx.send(content=imagem.url)
+
+
+@slash.slash(name="harley",
+             description="Escolho a Harley-Davidson que você terá daqui a 10 anos")
+async def harley(ctx):
+    msg = f'{ctx.author.mention}, sua Harley-Davidson é:'
+    await ctx.send(content=msg, embed=random_bike())
+
+
+@slash.slash(name="fraude",
+             description="Escolho uma fraude aleatória")
+async def fraude(ctx):
+    await ctx.send(content=random.choice(listas.fraudes))
+
+
+@slash.slash(name="fome",
+             description="Hora do lanche")
+async def fome(ctx):
+    await ctx.send(content=random.choice(listas.fome))
+
+
+@slash.slash(name="nota",
+             description="Diga uma cadeira e eu digo qual será sua nota",
+             options=[
+                 create_option(
+                     name="cadeira",
+                     description="Qual a cadeira?",
+                     option_type=3,
+                     required=True
+                 )
+             ])
+async def nota(ctx, cadeira: str):
+    aluno = ctx.author.mention
+    await ctx.send(content=nota_aleatoria(aluno, cadeira))
+
+
+@slash.slash(name="deletar",
+             description="Deletar uma imagem com base no index. Cuidado!",
+             options=[
+                 create_option(
+                     name="index",
+                     description="Id da imagem",
+                     option_type=3,
+                     required=True
+                 )
+             ])
+async def deletar(ctx, index: int):
+    await ctx.send(db.deletar_frase(index))
+
+
+# ----- Bot Prefix Commands ---------------------------------------------------
+
+@bot.command(brief='Pinga')
+async def ping(ctx):
+    """Ping the server to verify that it\'s listening for commands"""
+    await ctx.send('Pong!')
+
+
+@bot.command(brief='Adiciona imagem')
+async def add(ctx):
+    for filetype in FILETYPES:
+        if filetype in ctx.message.attachments[0].url:
+            db.inserir_frase(ctx.message.attachments[0].url)
+            await ctx.send("Imagem adicionada.")
+
+
+@bot.command(brief='Listar imagens')
+async def lista(ctx):
+    await ctx.send(list_images())
+
+
+# ----- Run -------------------------------------------------------------------
+
+if __name__ == '__main__':
+    sqlalchemy_declarative.criar_tabela()
+    db.importar_replit()
+    bot.run(os.getenv('TOKEN'))
